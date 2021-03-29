24fc4eb1018cd35a051556ac9bb25963839e0e1b
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-03-29 23:59:28 +0200

    Add some style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+const api = {
+  key: process.env.REACT_APP_API_KEY,
+  base: "api.openweathermap.org/data/2.5/",
+};
+
 function App() {
   return (
-    <div>
-      <h1>Let's build weather app</h1>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..."/>
+        </div>
+      </main>
     </div>
   );
 }
